b3b7053b1a2b19105845cb5e58a06876085c1cbd
Sergio Romero <sirjavi94@gmail.com>
2020-10-10 13:21:32 -0500

    Volví a seguir las video clases de Oscar Barajas profesor de platzi

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,7 +12,7 @@ import '../assets/styles/App.scss';
 const API = 'http://localhost:3000/initalState';
 
 
-const App = () => {
+const Home = () => {
  const InitialState = useInitialState(API);
   return (
     <div className='App'>
@@ -42,4 +42,4 @@ const App = () => {
     </div>
   );
 };
-export default App;
+export default Home;
